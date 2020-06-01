d50d379b1abc9d2d3a81f0edf367f3219dafc3eb
Žydrūnas Pavinkšnis <63241045+zydrunas-pavinksnis@users.noreply.github.com>
2020-06-01 13:41:23 +0300

    menesiu kintamasis

---
+++ js/table.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const months [
+    { month: 1, name: `Sausis` },
+    { month: 2, name: `Vasaris` },
+    { month: 3, name: `Kovas` },
+    { month: 4, name: `Balandis` },
+    { month: 5, name: `Gegužė` },
+    { month: 6, name: `Birželis` },
+    { month: 7, name: `Liepa` },
+    { month: 8, name: `Rugpjūtis` },
+    { month: 9, name: `Rugsėjis` },
+    { month: 10, name: `Spalis` },
+    { month: 11, name: `Lapkritis` },
+    { month: 12, name: `Gruodis` },
+];
